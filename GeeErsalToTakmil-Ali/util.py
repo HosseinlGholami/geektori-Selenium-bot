2ff85078defe5b993719cda547f1493d5d013364
Hossein Gholami <hosseingholami.1995@outlook.com>
2020-08-09 17:43:11 +0430

    snippets prepared

--- GeeErsalToTakmil-Ali/util.py
+++ GeeErsalToTakmil-Ali/util.py
@@ -3,37 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
 import pickle
 
+internet_speed=0.3
 
-internet_speed=1
+next_page_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[1]/div/div[2]/div[3]'
 
-TEXT='Salam'
+TNK_TXT='اگر از استیکر ها راضی بودید ما رو به دوستاتون معرفی کنید '
 
-darentezar_filter_Xpath='/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div/div[1]'
-Ersal_shode_Xpath='/html/body/div[1]/div/div[4]/div/div[1]/div[2]/div[2]/div/div[2]/div[3]'
-                 
-def costumer_Xpath(x):
-    return'/html/body/div[1]/div/div[4]/div/div[2]/div[1]/div/div[1]/div['+str(x)+']'
-
-close_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[2]/div[1]'
-
-
-
-ersal_payam_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[1]/div/div[3]/div/div/div[5]/div'
-                  
-Select_yes_ersal_payam_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/div[3]/div/div/div/div[2]'
-input_text_ersal_payam_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/div[4]/div/textarea'
-Done_Text_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div[2]'
+confirm_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[2]/div[1]'
+Takmil_Xpath='//*[@id="orderModalStatus"]/div/div/div[5]'
+edit_ersal_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[1]/div/div[3]/div/div/div[5]/div'
+edit_ersal_yes_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/div[3]/div/div/div/div[2]'
+input_edit_ersal_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/div[4]/div/textarea'
+confrim_edit_ersal_Xpath='/html/body/div[1]/div/div[4]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div[2]'
 
 
-def init(driver):    
-    driver.maximize_window()
-    with open("Geektori.ir", "rb") as fp:
-        file = pickle.load(fp)
+def init(driver):
     driver.get('https://geektori.ir/admin/orders')
     #loginPart
+    with open("Geektori.ir", "rb") as fp:
+            file = pickle.load(fp)
     INPUT_MAIL=file[0]
     PASS=file[1]
     InputMail=driver.find_element_by_id("username")
@@ -41,76 +31,67 @@ def init(driver):
     InputPass=driver.find_element_by_id("password")
     InputPass.send_keys(PASS)
     InputPass.send_keys(Keys.RETURN)
+    #define costomer_listapth
+    COUSTOMER_XPATH='/html/body/div[1]/div/div[4]/div/div[2]/div[1]/div/div[1]/div'
+    Page_item=[COUSTOMER_XPATH+'['+str(i)+']' for i in range(1,22) ]
+    return Page_item
+                
+def next_page(driver,page):
+    driver.find_element_by_xpath(next_page_Xpath).click()
+    return page + 1
+def open_consumer(driver,Page_item,index):
+    time.sleep(internet_speed)
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH,Page_item[index]))
+        ).click()
+def check_open_dar_entezar(driver,Page_item,i):
+    x=Page_item[i]+'/div[6]/div'
+    y=WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,x))
+        ).text
+    print(y)
+    return y=='ارسال شده'
 
+def scrab_details(driver):
+    time.sleep(internet_speed)
+    WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,Takmil_Xpath))
+            ).click()
+    time.sleep(internet_speed)
+    WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,edit_ersal_Xpath))
+            ).click()
+    time.sleep(internet_speed)
+    WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,edit_ersal_yes_Xpath))
+            ).click()
+    time.sleep(internet_speed)
+    input_edit_ersal=WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,input_edit_ersal_Xpath))
+            )
+    input_edit_ersal.send_keys(TNK_TXT)
+    time.sleep(internet_speed)
+    WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,confrim_edit_ersal_Xpath))
+            ).click()
+    time.sleep(internet_speed)
+    WebDriverWait(driver, 10).until(
+      EC.presence_of_element_located((By.XPATH,confirm_Xpath))
+      ).click()
 
-def Scarb(driver):
-    time.sleep(3)
-    #select_Ersal_Sohde
-    WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,darentezar_filter_Xpath))
-            ).click()
-    time.sleep(internet_speed)
-    WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,Ersal_shode_Xpath))
-            ).click()
-    time.sleep(internet_speed)
-    WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,darentezar_filter_Xpath))
-            ).click()
-    time.sleep(internet_speed)
-    #ScrabDetail
-    stop=False
-    i=1
-    while(stop==False):
-        i=i+1
-        try:
-            time.sleep(internet_speed)
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,costumer_Xpath(i)))
-                ).click()
-            time.sleep(internet_speed)
-#_____________________________________________________
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,Takmil_Xpath))
-                ).click()
-            time.sleep(internet_speed)
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,ersal_payam_Xpath))
-                ).click()
-            time.sleep(internet_speed)
-            
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,Select_yes_ersal_payam_Xpath))
-                ).click()
-            time.sleep(internet_speed)
-            texBox=WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,input_text_ersal_payam_Xpath
-                                                )))
-            texBox.send_keys(TEXT)
-            time.sleep(internet_speed)
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,Done_Text_Xpath))
-                ).click()
-            time.sleep(internet_speed)
-    
-#_____________________________________________________            
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH,close_Xpath))
-                ).click()
-            time.sleep(internet_speed)
-            
-        except:
-            stop=True
-            if i==21:
-                i=1
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH,nextPage_Xpath))
-                    ).click()
-       
-        
-        
-        
-    
-    
-    
-    
+#     if Name in sefareshat_list:
+#         WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH,ersal_Xpath))
+#             ).click()
+#         WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH,marsoole_Xpath))
+#             ).click()
+#         time.sleep(internet_speed)
+#         InputMarsoole=WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH,inputmarso_Xpath))
+#             )
+#         InputMarsoole.send_keys(sefareshat_list[Name][0])
+#         time.sleep(internet_speed)
+#         InputMarsoole.send_keys(Keys.RETURN)
+#         time.sleep(internet_speed)
+  
