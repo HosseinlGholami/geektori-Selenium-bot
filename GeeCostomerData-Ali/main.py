5e3efbf892cbb94354a2a990142459645c8848b0
Hossein Gholami <hosseingholami.1995@outlook.com>
2020-08-26 17:16:16 +0430

    export exe -clean sms

---
+++ GeeCostomerData-Ali/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from util import (init,Scarb)
+
+driver = webdriver.Chrome('./chromedriver.exe')
+#Xh : Excel handller ,DT:Date Time
+Wb,DT=init(driver)
+
+Scarb(driver,Wb)
+
+driver.quit()
+Wb.save(DT+'.xlsx')
---
+++ GeeUpload to site --Amin/Stakshen/main.py
@@ -0,0 +1,22 @@
+import os
+from selenium import webdriver
+from util import init , add_new_product
+
+from functools import reduce
+
+Worked_path=os.getcwd()
+Content=os.listdir()
+
+names=[name for name in Content if name.split('.')[-1]=='png']
+category = Worked_path.split('\\')[-1]
+price='2445'
+
+driver = webdriver.Chrome('./chromedriver.exe')
+init(driver)
+
+for name in names:
+    picPath=Worked_path+'\\'+name
+    Name=reduce(lambda x,y: x+y,name.split('.')[0:-1])
+    add_new_product(driver,Name,picPath,category,price)
+                
+driver.quit()
